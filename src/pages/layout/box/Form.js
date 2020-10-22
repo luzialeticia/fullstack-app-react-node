3f2c046fb0de0e4a5a1cacc38b5ee217102bf6a6
luzialeticia <leticialuzia@tutamail.com>
2020-10-22 19:01:24 -0300

    Renomeando o Button.js para Buttons e importando Buttons e Inputs para o Form

--- src/pages/layout/box/Form.js
+++ src/pages/layout/box/Form.js
@@ -1,11 +1,17 @@
 import React, { Component, Fragment } from 'react'
 
+//Components
+import Inputs from './form/Inputs'
+import Buttons from './form/Buttons'
+
 class Form extends Component {
   render() {
     return (
       <Fragment>
         <h1 className="title has-text-grey-dark">Create Task</h1>
         <form>
+          <Inputs />
+          <Buttons />
         </form>
       </Fragment>
     )
